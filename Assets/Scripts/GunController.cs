35cde0f7d42eef3f9c9c39bdf3dbdf5d1e78af07
johngalay <jadgalay@gmail.com>
2018-04-02 22:30:13 +0800

    Ammo and Reload
    -ammo
    -reload
    -gun variety and options

--- Assets/Scripts/GunController.cs
+++ Assets/Scripts/GunController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GunController : MonoBehaviour {
  
@@ -10,45 +11,122 @@ public class GunController : MonoBehaviour {
 	public float recoil = 2f;
 	public float range = 50f;
 
-	public float accuracy = 100f;
+	public float spreadAngle = 0f;
+
+	public int totalAmmo = 60; // total gun ammo
+	public int maxAmmo = 8; // max magazine ammo
+	private int currentAmmo;
+	public float reloadTime = 1f;
+	private bool isReloading = false;
+	private bool isEmpty = false;
 
 	public LayerMask whatToHit;
 
 	public BulletController bullet;
 	public float bulletSpeed;
 
-	public float timeBetweenShots;
-	private float shotCounter;
-
+	public float shootWaitTime = 0.5f;
+	private float timeLeft = 0f;
 	public Transform firePoint;
+
+	public Text currentAmmoText;
+	public Text totalAmmoText;
 
 	// Use this for initialization
 	void Start () {
+		currentAmmo = maxAmmo;
+	}
+
+	void OnEnable() {
+		isReloading = false;
+		isFiring = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if(Input.GetButtonDown("Fire1")) {
+
+		currentAmmoText.text = currentAmmo.ToString();
+		totalAmmoText.text = totalAmmo.ToString();
+
+		if(isEmpty) {
+			//Debug.Log("No Ammo"); 
+			return;
+		}
+
+		if(isReloading) {
+			return;
+		}
+
+		if(currentAmmo <= 0) {
+			StartCoroutine(Reload());
+			return;
+		}
+
+		timeLeft -= Time.deltaTime;
+		if(timeLeft < 0) {
+			timeLeft = 0;
+		}
+
+		if(Input.GetButton("Fire1") && timeLeft <=	 0) {
 			Shoot();
+			timeLeft = shootWaitTime;
 		}else {
 			isFiring = false;
 		}
-		//		Vector3 pointToLook = mainCamera.ScreenToWorldPoint(Input.mousePosition);
-		if(isFiring) {
-			shotCounter -= Time.deltaTime;
-			if(shotCounter <= 0) {
-				shotCounter = timeBetweenShots;
-				BulletController newBullet = Instantiate(bullet, firePoint.position, firePoint.rotation) as BulletController;
-				newBullet.speed = bulletSpeed;
-			}
-		}else {
-			shotCounter = 0;
-		}
-		
 	}
 	
 	void Shoot() {
 		isFiring = true;
+		currentAmmo--;
+		BulletController newBullet = Instantiate(bullet, firePoint.position, bulletRotation(spreadAngle)) as BulletController;
+		newBullet.speed = bulletSpeed;
+	}
+
+	// coroutine
+	IEnumerator Reload() {
+		
+		isReloading = true;
+		isFiring = false;
+
+		
+
+		if(totalAmmo <= 0) {
+			isEmpty = true;
+			Debug.Log("Failed to reload..."); // for debugging
+		} else {
+			Debug.Log("Reloading..."); // for debugging
+
+			yield return new WaitForSeconds(reloadTime - 0.25f);
+			
+			if(totalAmmo >= maxAmmo) {
+				totalAmmo -= maxAmmo;
+				currentAmmo = maxAmmo;
+			} else {
+				currentAmmo = totalAmmo;
+				totalAmmo = 0;
+			}
+		
+			yield return new WaitForSeconds(0.25f);
+			
+		}
+
+		isReloading = false;	
+		
+	}
+
+	Quaternion bulletRotation(float spreadAngle) {
+		spreadAngle = spreadAngle / 2;
+		Quaternion bulletRotation;
+		Vector3 sp = Camera.main.WorldToScreenPoint(transform.position);
+		Vector3 direction = (Input.mousePosition - sp).normalized;
+				
+		// Calculate the angle
+		float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+    	//float spread = Random.Range(-90, -90);  // no angle
+		//float spread = Random.Range(-45, -135); // 90 degree angle
+		float spread = Random.Range(-90 + spreadAngle, -90 - spreadAngle);
+		//Debug.Log(spread + 90); // for debugging
+		return bulletRotation =  Quaternion.Euler(new Vector3(0, 0, angle + spread));
 	}
 }
 
